ab84b7bc559e43c744849e38b95afef7f0e9d411
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-06-16 15:02:30 -0700

    Initial Commit

---
+++ Shared/swift_svgApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_svgApp.swift
+//  Shared
+//
+//  Created by SATOSHI NAKAJIMA on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_svgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
